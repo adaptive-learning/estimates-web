e9bc51b0095c3bb640857f8d7e832868b979e809
Orlo666 <Stone99@azet.sk>
2014-11-29 20:53:31 +0100

    typo

--- mysite/static/js/MathAssig.js
+++ mysite/static/js/MathAssig.js
@@ -6,13 +6,12 @@ function answer(answer,result){
 	var res = process(answer,result);
 	
 	
-	{
+	if(res){
 		var diff = result - answer.value;
 		
 		var a = document.getElementById("answer");
 		delet(a);
 		a.appendChild(document.createTextNode("správna odpoveď bola: " + result + " líšili ste sa o: " + diff));
 	}
-	
 	return false;
 }
