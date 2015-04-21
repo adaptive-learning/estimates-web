947a06243e559302e15256d0ed38181c2b6db5e1
Orlo666 <Stone99@azet.sk>
2015-04-21 10:45:37 +0200

    responsive timer

--- mysite/learning/static/js/time.js
+++ mysite/learning/static/js/time.js
@@ -153,5 +153,12 @@ function timerTime(response,fullTime){
 	timer('testTimer',fullTime,"#0F482D",true,get_result,start,end,start);
 }
 
-
+function resizeCounter(min){
+	alert("k");	
+	 	var newWidth = $(window).width()/25;
+	 	if (newWidth < min){
+ 			newWidth = min;
+	 	}
+		$("#pie-countdown").css("width",newWidth);
+}
 
